c9ef999b3d0b2efb3aa988dadc722aa3cf8af21f
algha3 <anaufalhz03@gmail.com>
2024-02-27 05:41:10 +0700

    Styling Navbar Component

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,108 @@
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  IconButton,
+  InputBase,
+  Badge,
+} from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import TuneIcon from "@mui/icons-material/Tune";
+import Logo from "../assets/Logo.png";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: 50,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  marginRight: theme.spacing(2),
+  display: "flex",
+  alignItems: "center",
+  border: "2px solid",
+  borderColor: theme.palette.common.black,
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: theme.palette.common.black,
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+export default function Navbar() {
+  return (
+    <AppBar sx={{ height: 124, backgroundColor: "white" }}>
+      <Toolbar sx={{ height: "100%" }}>
+        <img src={Logo} alt="Logo" style={{ width: "148px", height: "44px", margin:"0 60px" }} />
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+          <IconButton
+            aria-label="filter"
+            size="large"
+            sx={{ marginLeft: "auto" }}
+          >
+            <TuneIcon />
+          </IconButton>
+        </Search>
+        <Box sx={{ marginLeft: "auto" }}>
+          <Box
+            sx={{ display: { xs: "none", md: "flex" }, alignItems: "center" }}
+          >
+            <IconButton size="large" aria-label="show 4 new mails">
+                <FavoriteIcon />
+            </IconButton>
+            <IconButton size="large" aria-label="show 17 new notifications">
+              <Badge color="secondary" variant="dot">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-haspopup="true"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
